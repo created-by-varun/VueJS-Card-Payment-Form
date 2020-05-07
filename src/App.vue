90525f8b454e44b8f017d27bf906cd70e7baf94a
Varun V <varunvirupaksha321@gmail.com>
2020-05-07 17:58:43 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper" id="app">
+    <CardForm
+      :form-data="formData"
+      @input-card-number="updateCardNumber"
+      @input-card-name="updateCardName"
+      @input-card-month="updateCardMonth"
+      @input-card-year="updateCardYear"
+      @input-card-cvv="updateCardCvv"
+    />
+  </div>
+</template>
+
+<script>
+import CardForm from '@/components/CardForm'
+export default {
+  name: 'app',
+  components: {
+    CardForm
+  },
+  data () {
+    return {
+      formData: {
+        cardName: '',
+        cardNumber: '',
+        cardMonth: '',
+        cardYear: '',
+        cardCvv: ''
+      }
+    }
+  },
+  methods: {
+    updateCardNumber (val) {
+    },
+    updateCardName (val) {
+    },
+    updateCardMonth (val) {
+    },
+    updateCardYear (val) {
+    },
+    updateCardCvv (val) {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../src/assets/style.scss';
+</style>
